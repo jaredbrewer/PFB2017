757ff615cc04dfe5dd5cd8e1efcad9efb0dbd4c2
admin <admin@pfb12.cshl.edu>
2017-10-17 11:15:57 -0400

    Made ProblemSet3.py a little neater.

--- ProblemSet3/ProblemSet3.py
+++ ProblemSet3/ProblemSet3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-print("Jared Brewer")
-print("Green")
-print("Reading")
-print("Dog")
+print("My name is Jared Brewer")
+print("My favorite color is Green")
+print("I like to read")
+print("My favorite animals are dogs")
