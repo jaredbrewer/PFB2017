cbaaa80bbb1accdcf19c3ba903dbbfded171fbe3
admin <admin@pfb12.cshl.edu>
2017-10-17 11:09:49 -0400

    Completed Problem Set #3 - introduction to Python

---
+++ ProblemSet3/ProblemSet3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+print("Jared Brewer")
+print("Green")
+print("Reading")
+print("Dog")
---
+++ ProblemSet3/Retrieve.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+a = sys.argv[1]
+b = sys.argv[2]
+c = sys.argv[3]
+d = sys.argv[4]
+print(a,b,c,d)
